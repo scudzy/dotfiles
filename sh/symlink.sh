73db01de36df253490caf9f4b7df485ccd6f4acc
scudzys <scudzy@icloud.com>
2021-01-09 00:45:45 +0800

    Add symlink.sh

---
+++ sh/symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -s ~/.aliases ~/dotfiles/.aliases
+ln -s ~/.bashrc ~/dotfiles/.bashrc
+ln -s ~/.gemrc ~/dotfiles/.gemrc
+ln -s ~/.profile ~/dotfiles/.profile
+ln -s ~/.tmux.conf ~/dotfiles/.tmux.conf
+ln -s ~/.vimrc ~/dotfiles/.vimrc
+ln -s ~/.zshrc ~/dotfiles/.zshrc
+ln -s /etc/wsl.conf ~/dotfiles/etc/wsl.conf
