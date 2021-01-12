e9fe9f07fb2c99ed71c77b6a745e090ac52e8e61
scudzys <scudzy@icloud.com>
2021-01-13 00:03:28 +0800

    Updated symlink.sh

--- sh/symlink.sh
+++ sh/symlink.sh
@@ -2,8 +2,10 @@
 ln -s ~/dotfiles/.aliases ~/.aliases
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gemrc ~/.gemrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 sudo ln -s ~/dotfiles/etc/wsl.conf /etc/wsl.conf
+cp ~/dotfiles/.gitconfig.user ~/.gitconfig.user
