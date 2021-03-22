5fb3a3219f7b7620336d001dea6cc4d15f0f0b97
scudzy <scudzy@icloud.com>
2021-03-22 23:54:18 +0800

    Modified gitclonerepo symlink Ubuntu-20.04 22-Mar-2351

--- sh/gitclonerepo.sh
+++ sh/gitclonerepo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ## Clone dotfiles
-git clone https://github.com/scudzy/dotfiles.git ~/dotfiles       \\***
+git clone https://github.com/scudzy/dotfiles.git ~/dotfiles
 ## Clone oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ## Clone omz powerlevel10k
@@ -12,13 +12,12 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+## Cloning tmux TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ## Clone fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 ## Clone plug.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-## Ubuntu wsl2 snapd script
-git clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git && bash ~/ubuntu-wsl2-systemd-script/ubuntu-wsl2-systemd-script.sh
 echo ""
 echo "Git Cloning Completed"
 echo ""
--- sh/symlink.sh
+++ sh/symlink.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+mv ~/.bashrc ~/.bashrc.old
+mv ~/.profile ~/.profile.old
 mkdir -p ~/.local/share/fonts
 ln -s ~/dotfiles/.aliases ~/.aliases
 ln -s ~/dotfiles/.bashrc ~/.bashrc
@@ -11,3 +13,4 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 sudo ln -s ~/dotfiles/etc/wsl.conf /etc/wsl.conf
 cp -i ~/dotfiles/.gitconfig.user ~/.gitconfig.user
 cp -r ~/dotfiles/.local/share/fonts/Meslo\ LG\ S\ Regular\ Nerd\ Font\ Complete.ttf ~/.local/share/fonts/
+fc-cache -Ev
