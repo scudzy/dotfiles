eafea61ddb1163842801757526f30159b9aeebe1
scudzy <scudzy@icloud.com>
2021-04-09 22:47:13 +0800

    :heavy_sign_plus: Get file information

---
+++ sh/file-test.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+file="$1"
+
+if [ -r $file ]
+then
+    echo "File has read access"
+else
+    echo "File does not have read access"
+fi
+
+if [ -w $file ]
+then
+    echo "File has write permission"
+else
+    echo "File does not have write permission"
+fi
+
+if [ -x $file ]
+then
+    echo "File has execute permission"
+else
+    echo "File does not have execute permission"
+fi
+
+if [ -f $file ]
+then
+    echo "File is an ordinary file"
+else
+    echo "This is special file"
+fi
+
+if [ -d $file ]
+then
+    echo "File is a directory"
+else
+    echo "This is not a directory"
+fi
+
+if [ -s $file ]
+then
+    echo "File size is not zero"
+else
+    echo "File size is zero"
+fi
+
+if [ -e $file ]
+then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi
+
+if [ -L $file ]
+then
+    echo "File is a symbolic link"
+else
+    echo "File is not a symbolic link"
+fi
