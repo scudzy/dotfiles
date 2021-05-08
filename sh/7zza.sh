dce994890ffc77edbbd347431a788ccc2fbec7ed
scudzy <22201569+scudzy@users.noreply.github.com>
2021-05-09 07:46:01 +0800

    :wrench: Update nothing

--- sh/7zza.sh
+++ sh/7zza.sh
@@ -1,19 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # 7zz scripts
 start="$(date +%s)"
-printf "Please enter a filename [specify fullpath]: "
+### start of scripts
+echo -n "Please enter a filename [specify fullpath]: "
 read -r filename
-#nline=$filename
 target="$1"
 datetime="$(date +"%d%m%Y-%H%M")"
 file="$filename-$datetime.7z"
-printf "\n"
-printf "Zipping \e[0;31m$file\e[0m"
+echo ""
+echo -e "Zipping \e[0;31m$file\e[0m\n"
 7zz a -t7z "$file" -m0=lzma2 -mmt=2 -mx=5 -p065195 -mhe=on -bt "$target" '-xr!.git' '-ax!*.7z'
 7zz h -scrcsha256 "$file"
+sha256sum "$file" > "$file.sha256"
 
 # Execution time
 end="$(date +%s)"
 total="$(( end - start ))"
-printf "Running in \033[1;32m""$total""\033[0m seconds\n"
+echo -e "Running in \033[1;32m""$total""\033[0m seconds"
