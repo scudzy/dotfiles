8de3ae80aee6c9f430e7d7e9ac55a303fafa061b
scudzy <22201569+scudzy@users.noreply.github.com>
2023-10-20 20:35:11 +0800

    :art: Modify XDG_RUNTIME_DIR

--- zsh/xdg.zsh
+++ zsh/xdg.zsh
@@ -9,7 +9,6 @@ export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_DIRS="/etc/xdg"
 
