24e8c7cb3e80f05a5ac669e82621075a3a41a71e
scudzy <22201569+scudzy@users.noreply.github.com>
2023-11-21 23:40:55 +0800

    :heavy_plus_sign: Add sh scripts templates

---
+++ sh/template.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+############################################################
+# Filename   : x                                           #
+# Author     : x                                           #
+# Created    : x                                           #
+# Last edit  : x                                           #
+# Purpose    : x                                           #
+# Reference  : x                                           #
+# Depends    : x                                           #
+# Arguments  : x                                           #
+# Known bugs : x                                           #
+# To do      : x                                           #
+############################################################
+
+# GNU All-Permissive License {{{
+#############################################################
+# Copyright © 2023 scudzy                                   #
+#                                                           #
+# Copying and distribution of this file, with or without    #
+# modification, are permitted in any medium without         #
+# royalty, provided the copyright notice and this notice    #
+# are preserved.                                            #
+#                                                           #
+# This file is offered as-is, without any warranty.         #
+#############################################################
+# End license }}}
